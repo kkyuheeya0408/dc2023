25321d5af5b791e31a726c0c5f66ec430e76bbc2
kkyuheeya0408 <128437363+kkyuheeya0408@users.noreply.github.com>
2023-06-20 05:31:58 +0900

    15w

---
+++ w15/js/intersectionObserver.js
@@ -0,0 +1,22 @@
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: [0.75],
+};
+
+const callback = (aaaaaaaaaa, observer) => {
+  aaaaaaaaaa.forEach((trigger) => {
+    if (trigger.isIntersecting) {
+      trigger.target.classList.add("triggered");
+    } else {
+      trigger.target.classList.remove("triggered");
+    }
+  });
+};
+
+const gggggg = new IntersectionObserver(callback, options);
+
+const animTriggers = document.querySelectorAll(".animTrigger");
+animTriggers.forEach((aaaa) => {
+  gggggg.observe(aaaa);
+});
